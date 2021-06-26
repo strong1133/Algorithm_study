045db496ac7beb1cf34b43e6e8f037ea8c7e1cf7
[strong1133] <strong1133@naver.com>
2021-06-26 13:38:41 +0900

    프로그래머스 문자열다루기 자바 풀이

---
+++ programmers/java/level01/problem01.java
@@ -0,0 +1,22 @@
+
+// 2021-06-26 프로그래머스 level01 - 문자열 다루기 기본
+
+public class problem01 {
+
+    public static boolean solution(String s) {
+        if (s.length() == 4 || s.length() == 6) {
+            try {
+                int x = Integer.parseInt(s);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        } else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        boolean ans = solution("1111");
+        System.out.println(ans);
+    }
+}
