526a9bb8c57c5e24b99d97d2102864e48b77eaa0
[strong1133] <strong1133@naver.com>
2021-07-04 21:14:40 +0900

    2021-07-04 벡준_단계:08 == 1712번 / 손익분기점

--- bj/level08/problem01.py
+++ bj/level08/problem01.py
@@ -10,3 +10,9 @@ def solve(a, b, c):
 a, b, c = map(int, input().split())
 
 print(solve(a, b, c))
+
+#  a + b * n = c * n
+#  a + bn = cn
+#  a = cn - bn
+#  a= (c-b)n
+#  n = a / c-b  => 이 경우 0이므로 +1 해줘야 수익이 시작했다 볼수 있음.
