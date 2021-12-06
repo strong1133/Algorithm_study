08a628de0039f9d41dccc696f1e27f944be1c98e
strong1133 <strong1133@naver.com>
2021-12-07 00:26:40 +0900

    211206 소수구하기

--- daily/2021/12/1206_P12977.py
+++ daily/2021/12/1206_P12977.py
@@ -1,8 +1,25 @@
 # 2021-12-06 프로그래머스 Level01 소수 만들기
+from itertools import combinations
 
 
-def solution():
-    print('시작해보자구')
+def is_prime(x):
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
 
 
-solution()
+def solution(a):
+
+    ans = 0
+    arr = list(combinations(a, 3))
+    print(arr)
+    for n1, n2, n3 in arr:
+        if is_prime(n1+n2+n3):
+            ans += 1
+    return ans
+
+
+a = [1, 2, 3, 4]
+b = [1, 2, 7, 6, 4]
+print(solution(b))
