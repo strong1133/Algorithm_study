538a84784ee7fea64974cbf3619ac21a79b39fdb
strong1133 <strong1133@naver.com>
2022-01-05 10:03:58 +0900

    22-01-05 프로그래머스 level01 K번째 수 JS

--- daily/2022/01/0105_P42748.js
+++ daily/2022/01/0105_P42748.js
@@ -16,3 +16,12 @@ function solution(array, commands) {
 let array = [1, 5, 2, 6, 3, 7, 4]
 let commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 solution(array, commands)
+
+
+
+/**
+ * sort() 방식은 문자열 기준 아스키 코드로 정렬한다.
+ * 따라서 숫자를 정렬 할때는 sort((a,b)=> a-b) 다음과 같이 처리
+ * sort((a,b)=> a-b) 오름차순
+ * sort((a,b)=> b-a) 내림차순 
+ */
