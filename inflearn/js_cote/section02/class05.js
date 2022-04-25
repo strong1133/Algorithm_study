ff710a04b460bdcd87cc65d9ea11e3877e87bf2d
strong1133 <strong1133@naver.com>
2022-04-25 09:07:44 +0900

    2022-04-21 2-4강 <점수 계산>

---
+++ inflearn/js_cote/section02/class05.js
@@ -0,0 +1,21 @@
+// 2022-04-21 2-4강 <점수 계산>
+
+
+const solution =(arr)=>{ 
+    let pre=0;
+    let score=0;
+    
+    for (i of arr){ 
+        if (i ==0){
+            pre = 0;
+        }else{
+            pre += i
+        }
+        score += pre;
+    }
+    return score
+}
+
+
+let arr= [1,0,1,1,1,0,0,1,1,0]
+console.log(solution(arr))
