9711e5ab487b232e4b459285dfc597196100f4d3
strong1133 <strong1133@naver.com>
2022-05-03 08:39:54 +0900

    2022-04-28 2-7강 <봉우리> 복습

--- inflearn/js_cote/section02/class07.js
+++ inflearn/js_cote/section02/class07.js
@@ -1,31 +1,29 @@
 // 2022-04-28 2-7강 <봉우리>
-
-
 const solution =(arr)=>{ 
     let ans =0;
     let n = arr.length;
 
-    let dx = [-1,0,1,0]
-    let dy = [0,1,0,-1] 
+    let dx = [-1,0,1,0];
+    let dy = [0,1,0,-1];
 
-    for(let i = 0; i<n;i++){
-        for(let j=0; j<n; j++){
+    for(let i = 0; i <n; i++){
+        for (let j =0; j<n; j++){
             let flag = 1;
             for(let k=0; k<4; k++){
                 let nx = i +dx[k];
                 let ny = j +dy[k];
 
-                if (nx >=0 && ny >=0 &&nx < n && ny< n  && arr[nx][ny] >= arr[i][j]){
+                if (nx >= 0 && ny >=0 &&  nx<n && ny<n &&arr[nx][ny] >= arr[i][j]){
                     flag = 0;
                     break;
                 }
 
-         
             }
-            if(flag) ans++
+            if(flag) ans ++
         }
     }
     return ans
+    
 
 }
 
