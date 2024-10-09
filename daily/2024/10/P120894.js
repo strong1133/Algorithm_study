53b8f862d977572c9585e0eda3276b235f9ce10e
strong3879 <strong3879@gmail.com>
2024-10-10 01:21:39 +0900

    #2024-10-10 :: 프로그래머스 - 영어가 싫어요

---
+++ daily/2024/10/P120894.js
@@ -0,0 +1,21 @@
+// #2024-10-10 :: 프로그래머스 - 영어가 싫어요
+function solution(numbers) {
+
+  let arr = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+  let answer = "";
+
+  for(let str of arr){
+    
+    numbers = numbers.replaceAll(str, arr.indexOf(str));
+
+
+  }
+
+  console.log(parseInt(numbers));
+
+  return answer;
+}
+
+console.log(solution("onetwothreefourfivesixseveneightnine"));
+console.log(solution("onefourzerosixseven"));
