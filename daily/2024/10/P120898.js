fcfed143e68ed1e9fe946be753d59d263854d793
strong3879 <strong3879@gmail.com>
2024-10-14 10:26:57 +0900

    #2024-10-14 :: 프로그래머스 - 편지

---
+++ daily/2024/10/P120898.js
@@ -0,0 +1,11 @@
+// #2024-10-14 :: 프로그래머스 - 편지
+function solution(message) {
+  let answer = 0;
+
+  answer = message.length * 2;
+
+  return answer;
+}
+
+console.log(solution("happy birthday!"));
+console.log(solution("I love you~"));
