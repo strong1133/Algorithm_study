5ea0d69bca950f4232581b625c02335e5ce08250
strong3879 <strong3879@gmail.com>
2024-11-08 08:51:58 +0900

    #2024-11-04 :: 프로그래머스 - 직사각형의 넓이 구하기

--- daily/2024/11/P120860.js
+++ daily/2024/11/P120860.js
@@ -1,8 +1,8 @@
 // #2024-11-04 :: 프로그래머스 - 직사각형의 넓이 구하기
 
 function solution(dots) {
-  var answer = 0;
-  return answer;
+  const result = Math.abs((dots[1][0] - dots[0][0]) * (dots[2][1] - dots[1][1]))
+  return result
 }
 
 console.log(solution([1, 1, 2, 3, 4, 5],1));
