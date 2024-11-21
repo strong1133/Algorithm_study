6580906080426c79aeb04a728155ecd81172d2da
strong3879 <strong3879@gmail.com>
2024-11-21 13:36:17 +0900

    #2024-11-20 :: 프로그래머스 - 안전지대

--- daily/2024/11/P120866.js
+++ daily/2024/11/P120866.js
@@ -1,15 +1,26 @@
 // #2024-11-20 :: 프로그래머스 - 안전지대
-function solution(board) {
-  let answer = 0;
 
+function solution(b) {
+    const directions = [[0,0],[0,1],[0,-1],[1,1],[1,0],[1,-1],[-1,-1],[-1,0],[-1,1]]
+    let bombSet = new Set();
 
-  return answer;
+    for(let i = 0; i < b.length; i++) {
+        for(let j = 0; j < b[i].length; j++) {
+            if(b[i][j] == 1) {
+                directions.forEach(el => {
+                    let [nextX, nextY] = el;
+                    [nextX, nextY] = [i+nextX, j+nextY];
+                    if(nextX >= 0 && nextX < b.length && nextY >= 0 && nextY < b[i].length) {
+                        bombSet.add(nextX+' '+nextY);
+                    }
+                })
+            }
+        }
+    }
+    return b.length * b[0].length - bombSet.size;
 }
 
 
-// function solution(my_string) {
-//   return my_string.split(/\D+/).reduce((acc, cur) => acc + Number(cur), 0);
-// }
 
 
 console.log(solution([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]]));
