cfd2db6ab570f9442eee4d1d21b86e1f4ce9cc43
strong3879 <strong3879@gmail.com>
2024-12-13 09:49:04 +0900

    #2024-12-13 :: 백준 - ??!

---
+++ daily/2024/12/B10926.js
@@ -0,0 +1,15 @@
+// #2024-12-13 :: 백준 - ??!
+
+
+function solution() {
+const fs = require("fs");
+
+// const input = fs.readFileSync("/Users/jsj/source/personal/study/Algorithm_study/daily/2024/11/test/b1000.txt").toString().split(' ');
+// const input ='joonas';
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+console.log(input + '??!');
+}
+
+
+solution();
