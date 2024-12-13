3b404d4869db6d09cca89e18d9d76ffa6852f722
strong3879 <strong3879@gmail.com>
2024-12-13 09:58:58 +0900

    #2024-12-13 :: 백준 - 태국나이

---
+++ daily/2024/12/B18108.js
@@ -0,0 +1,15 @@
+// #2024-12-13 :: 백준 - 태국나이
+
+
+function solution() {
+const fs = require("fs");
+
+// const input = fs.readFileSync("/Users/jsj/source/personal/study/Algorithm_study/daily/2024/11/test/b1000.txt").toString().split(' ');
+// const input =2541;
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+console.log(input - 543);
+}
+
+
+solution();
