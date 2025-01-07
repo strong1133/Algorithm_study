57494cf53ed6b2a13e08c4193b7144cff24efbc5
strong3879 <strong3879@gmail.com>
2025-01-07 09:32:20 +0900

    #2025-01-07 :: 백준 - 꼬마 정민

---
+++ daily/2025/01/B11382.js
@@ -0,0 +1,18 @@
+// #2025-01-07 :: 백준 - 꼬마 정민
+
+
+function solution() {
+const fs = require("fs");
+
+// const input = fs.readFileSync("/Users/jsj/source/personal/study/Algorithm_study/daily/2024/11/test/b1000.txt").toString().split(' ');
+const input = fs.readFileSync("/dev/stdin").toString().split(' ');
+
+
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+
+console.log(A + B);
+}
+
+
+solution();
