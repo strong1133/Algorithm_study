16019c0e25be993740713c5c73580beed180f86c
strong3879 <strong3879@gmail.com>
2025-01-08 09:39:10 +0900

    #2025-01-08 :: 백준 - 고양이

---
+++ daily/2025/01/B10171.js
@@ -0,0 +1,15 @@
+// #2025-01-08 :: 백준 - 고양이
+
+
+function solution() {
+
+
+console.log(`\\    /\\`);
+console.log(` )  ( ')`);
+console.log(`(  /  )`);
+console.log(` \\(__)|`);
+
+}
+
+
+solution();
