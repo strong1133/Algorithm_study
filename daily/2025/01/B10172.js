cc2be1deeb60100a2e0f8b8ca14d32df72efbe92
strong3879 <strong3879@gmail.com>
2025-01-10 09:39:51 +0900

    #2025-01-10 :: 백준 - 개

---
+++ daily/2025/01/B10172.js
@@ -0,0 +1,16 @@
+// #2025-01-10 :: 백준 - 개
+
+
+function solution() {
+
+
+console.log(`|\\_/|`);
+console.log(`|q p|   /}`);
+console.log(`( 0 )"""\\`);
+console.log('|"^"`    |');
+console.log(`||_/=\\\\__|`);
+
+}
+
+
+solution();
